d0bd8b576c9753dfc7255266fcf0323af647f433
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2016-09-28 11:47:36 +0200

    redo path for plugin formcreator

--- scripts/news.js
+++ scripts/news.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
 
+   var ajax_baseurl = '../plugins/news/ajax';
+   var path = document.location.pathname;
+   // construct url for plugin pages
+   if(path.indexOf('plugins/') !== -1) {
+      var plugin_path = path.substring(path.indexOf('plugins'));
+      var nb_directory = (plugin_path.match(/\//g) || []).length + 1;
+      var ajax_baseurl = Array(nb_directory).join("../") + 'plugins/news/ajax';
+   }
+
    pluginNewsCloseAlerts = function() {
       $(document).on("click", "a.plugin_news_alert-close",function() {
          var alert = $(this).parent(".plugin_news_alert");
          var id    = alert.attr('data-id');
-         $.post("../plugins/news/ajax/hide_alert.php", {'id' : id})
+         $.post(ajax_baseurl+"/hide_alert.php", {'id' : id})
             .done(function() {
                alert.remove();
             });
@@ -19,7 +28,7 @@ $(document).ready(function() {
       // page create ticket
       $("#page > form[name=helpdeskform]").prepend("<div id='alerts_inserted'></div>");
 
-      $("#alerts_inserted").load("../plugins/news/ajax/display_alerts.php");
+      $("#alerts_inserted").load(ajax_baseurl + "/display_alerts.php");
    }
 
    if (window.location.href.indexOf("helpdesk.public.php") > 0) {
