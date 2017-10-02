f0df850cbc7e716353fa96545a323eabde2dca3b
Thierry Bugier Pineau <tbugier@teclib.com>
2017-10-02 10:38:57 +0200

    improve fix

--- js/news.js
+++ js/news.js
@@ -1,11 +1,13 @@
-var ajax_baseurl = '../plugins/news/ajax';
-var path = document.location.pathname;
-// construct url for plugin pages
-if (path.indexOf('plugins/') !== -1) {
-   var plugin_path = path.substring(path.indexOf('plugins'));
-   var nb_directory = (plugin_path.match(/\//g) || []).length + 1;
-   var ajax_baseurl = Array(nb_directory).join("../") + 'plugins/news/ajax';
-}
+$(document).ready(function() {
+   var ajax_baseurl = '../plugins/news/ajax';
+   var path = document.location.pathname;
+   // construct url for plugin pages
+   if (path.indexOf('plugins/') !== -1) {
+      var plugin_path = path.substring(path.indexOf('plugins'));
+      var nb_directory = (plugin_path.match(/\//g) || []).length + 1;
+      var ajax_baseurl = Array(nb_directory).join("../") + 'plugins/news/ajax';
+   }
+});
 
 pluginNewsCloseAlerts = function() {
    $(document).on("click", "a.plugin_news_alert-close",function() {
