0dd83f0adc52feb44bef13bc9502dfc402dd8457
Cédric Anne <cedric.anne@gmail.com>
2019-05-02 12:59:56 +0200

    Add 1.5.2 version definition

--- setup.php
+++ setup.php
@@ -21,7 +21,7 @@
  --------------------------------------------------------------------------
 */
 
-define ('PLUGIN_NEWS_VERSION', '1.5.1');
+define ('PLUGIN_NEWS_VERSION', '1.5.2');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_NEWS_MIN_GLPI", "9.4");
