4d8a50ff2d59424df6a6b449529fac3eed8e56b4
Cédric Anne <cedric.anne@gmail.com>
2019-11-14 15:56:52 +0100

    Add GLPI 9.5 compatibility
    * Replace usage of deprecated 'DBMysql::list_fields()'
    * Use TIMESTAMP instead of DATETIME in DB for new installations
    * Add marketplace compatibility
    * Add icon for Alert itemtype
    * Use GLPI 9.5 min required PHP version
    * Add 1.7.0 version definition

--- hook.php
+++ hook.php
@@ -30,11 +30,11 @@ function plugin_news_install() {
       $DB->query("
          CREATE TABLE IF NOT EXISTS `glpi_plugin_news_alerts` (
          `id`                   INT NOT NULL AUTO_INCREMENT,
-         `date_mod`             DATETIME NOT NULL,
+         `date_mod`             TIMESTAMP NOT NULL,
          `name`                 VARCHAR(255) NOT NULL,
          `message`              TEXT NOT NULL,
-         `date_start`           DATETIME DEFAULT NULL,
-         `date_end`             DATETIME DEFAULT NULL,
+         `date_start`           TIMESTAMP NULL DEFAULT NULL,
+         `date_end`             TIMESTAMP NULL DEFAULT NULL,
          `type`                 INT NOT NULL,
          `is_deleted`           TINYINT(1) NOT NULL DEFAULT 0,
          `is_displayed_onlogin` TINYINT(1) NOT NULL,
@@ -112,7 +112,7 @@ function plugin_news_install() {
    }
 
    // fix is_default default value
-   $alert_fields = $DB->list_fields("glpi_plugin_news_alerts");
+   $alert_fields = $DB->listFields('glpi_plugin_news_alerts');
    if ($alert_fields['is_deleted']['Default'] !== '0') {
       $migration->changeField("glpi_plugin_news_alerts",
                            "is_deleted", "is_deleted",
@@ -122,10 +122,10 @@ function plugin_news_install() {
    // end/start dates can be null
    $migration->changeField("glpi_plugin_news_alerts",
                            "date_end", "date_end",
-                           "DATETIME DEFAULT NULL");
+                           "TIMESTAMP NULL DEFAULT NULL");
    $migration->changeField("glpi_plugin_news_alerts",
                            "date_start", "date_start",
-                           "DATETIME DEFAULT NULL");
+                           "TIMESTAMP NULL DEFAULT NULL");
 
    if ($DB->fieldExists("glpi_plugin_news_alerts", "profiles_id")) {
       // migration of direct profiles into targets table
--- js/news.js
+++ js/news.js
@@ -1,21 +1,8 @@
-pluginNewsGetBaseUrl = function() {
-   var ajax_baseurl = '../plugins/news/ajax';
-   var path = document.location.pathname;
-   // construct url for plugin pages
-   if (path.indexOf('plugins/') !== -1) {
-      var plugin_path = path.substring(path.indexOf('plugins'));
-      var nb_directory = (plugin_path.match(/\//g) || []).length + 1;
-      ajax_baseurl = Array(nb_directory).join("../") + 'plugins/news/ajax';
-   }
-
-   return ajax_baseurl;
-};
-
 pluginNewsCloseAlerts = function() {
    $(document).on("click", "a.plugin_news_alert-close",function() {
       var alert = $(this).parent(".plugin_news_alert");
       var id    = alert.attr('data-id');
-      var ajax_baseurl = pluginNewsGetBaseUrl();
+      var ajax_baseurl = CFG_GLPI.root_doc+"/"+GLPI_PLUGINS_PATH.news+"/ajax";
       $.post(ajax_baseurl+"/hide_alert.php", {'id' : id})
          .done(function() {
             alert.remove();
