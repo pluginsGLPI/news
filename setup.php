7c05880deb1447b939fdb2d67dce84759f8cec8e
Cédric Anne <cedric.anne@gmail.com>
2022-05-03 08:47:07 +0200

    1.10.2

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define ('PLUGIN_NEWS_VERSION', '1.10.1');
+define ('PLUGIN_NEWS_VERSION', '1.10.2');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_NEWS_MIN_GLPI", "10.0.0");
