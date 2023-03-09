2c2cac418ef40f88dda9ebd978e8fb77ea9af836
Cédric Anne <cedric.anne@gmail.com>
2023-03-09 12:06:34 +0100

    Fix copyright year

--- ajax/hide_alert.php
+++ ajax/hide_alert.php
@@ -22,7 +22,7 @@
  * You should have received a copy of the GNU General Public License
  * along with News. If not, see <http://www.gnu.org/licenses/>.
  * -------------------------------------------------------------------------
- * @copyright Copyright (C) 2015-2022 by News plugin team.
+ * @copyright Copyright (C) 2015-2023 by News plugin team.
  * @license   GPLv2 https://www.gnu.org/licenses/gpl-2.0.html
  * @link      https://github.com/pluginsGLPI/news
  * -------------------------------------------------------------------------
